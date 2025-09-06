b989f83983013dc46847f52c39db1508b946d31f
Wachella <optictitanmc@gmail.com>
2025-09-06 18:47:01 -0500

    user-auth-imp
    Full imp of role-based authentication using JWT auth

---
+++ IMASS/IMASS/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IMASS.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //This is where we are going to put any other data associated to the User that isnt already included in AspNetCore
+        //Any added values will need a Migration to update the AspNetCoreUsers table
+        public string Name { get; set; }
+    }
+}
---
+++ IMASS/IMASS/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMASS.Models.DTOs
+{
+    //Structure or Model for the Logging in of a user
+    public class LoginDTO
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
