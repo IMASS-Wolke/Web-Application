9adac54ce979850e2b398998c15e5dbdcded46a8
Cameron <cameron.branch@selu.edu>
2025-09-05 19:19:49 -0500

    Initial commit for frontend
    Added:
    - Routing
    - Login page and landing page
    - Routes are "/" and "/login"

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        THIS IS LANDING PAGE
+      </header>
+    </div>
+  );
+}
+
+export default Home;
