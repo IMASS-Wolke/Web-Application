9ac1fb26ba74d4009bedf0cb843ddb25f0b0d3f1
Cameron <cameron.branch@selu.edu>
2025-09-08 17:22:54 -0500

    Added Login Form and styles
    Still need to connect to backend for login.

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -9,15 +9,39 @@ function Login() {
 
   return (
     <div className="Login">
-      <>
-        <GoogleLogin 
-          onSuccess={ () => { 
-            console.log("Login success")
-            navigate("/")
-          }} 
-          onError={ () => console.log("Login failed") }
-        />
-      </>
+      <div className="Login-container">
+        <form className="Login-info">
+          <div className="Email-container">
+            <div className="Login-email-header">
+              <text>Email Address</text>
+            </div>
+            <input className="email" type="text"/>
+          </div>
+          <div className="Password-container">
+            <div className="Login-password-header">
+              <text>Password</text>
+              <button className="Forgot-password-button">Forgot password?</button>
+            </div>
+            <input className="password" type="text"/>
+          </div>
+          <button className="Login-button" type="submit">Sign in</button>
+        </form>
+      <div className="Login-divider">
+        <div className="divider"/>
+        <text className="or-text">or</text>
+        <div className="divider"/>
+      </div>
+        <div>
+          <GoogleLogin
+            className="Google-login"
+            onSuccess={ () => { 
+              console.log("Login success")
+              navigate("/")
+            }} 
+            onError={ () => console.log("Login failed") }
+          />
+        </div>
+      </div>
     </div>
   );
 }
