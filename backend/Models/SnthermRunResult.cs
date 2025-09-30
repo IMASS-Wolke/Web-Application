6e9e3e1e7ec9918bf9442840b9ca30ccc8b0a4b7
Dennis Crayton <ethanc.xyz@gmail.com>
2025-09-29 22:31:30 -0500

    sntherm_wrap_init
    working wrapper for sntherm, accesses docker image sntherm-job:1.0.0

---
+++ backend/Models/SnthermRunResult.cs
@@ -0,0 +1,5 @@
+﻿namespace IMASS.Models
+{
+    public sealed record SnthermRunResult(string runId, int exitCode, string StandardOutput, string WorkDir, string ResultsDir, string StandardError, string[] Outputs);
+
+}
