8ca0400a185e18eb1c21722d17ed03daf988b8cf
Cameron <cameron.branch@selu.edu>
2025-10-06 23:07:49 -0500

    Added file upload (WIP)

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,16 @@
 import './Home.css';
 
+import Upload from '../Upload/Upload.js';
+
 function Home() {
   return (
     <div className="Home">
       <header className="Home-header">
         Welcome to IMASS
       </header>
+      <div className="Upload-container">
+        <Upload />
+      </div>
     </div>
   );
 }
