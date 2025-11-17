bea5e3df595c67a397930c37f2cc0b62dacda6cc
Dennis Crayton <ethanc.xyz@gmail.com>
2025-11-17 17:32:11 -0600

    scenBuilder_node_imp
    -took the all in one ScenarioBuilder and broke it up into multiple endpoints
     for more control in each node of frontend builder.

--- backend/Controllers/ScenarioBuilderController.cs
+++ backend/Controllers/ScenarioBuilderController.cs
@@ -70,7 +70,73 @@ namespace IMASS.Controllers
 
         }
 
+        public sealed class CreateScenarioForm
+        {
+            [FromForm(Name = "scenario_name")] public string? scenarioName { get; set; }
+        }
 
+        [HttpPost("create-scenario")]
+        public async Task<IActionResult> CreateScenario(CreateScenarioForm form, CancellationToken ct)
+        {
+            if (string.IsNullOrWhiteSpace(form.scenarioName))
+            {
+                int i = 1;
+                form.scenarioName = $"Scenario {i++}";
+            }
+            var (scenario, chain) = await _builder.CreateScenarioAndChainAsync(form.scenarioName, ct);
+            return Ok(new
+            {
+                ScenarioId = scenario.Id,
+                ChainId = chain.Id,
+            });
+        }
+
+        public sealed class JobRunForm
+        {
+            [FromForm(Name = "model_name")] public required string ModelName { get; set; }
+            [FromForm(Name = "inputFile1")] public IFormFile? inputFile1 { get; set; }
+            [FromForm(Name = "inputFile2")] public IFormFile? inputFile2 { get; set; }
+            [FromForm(Name = "scenario_id")] public required Guid? ScenarioId { get; set; }
+            [FromForm(Name = "chain_id")] public required Guid? ChainId { get; set; }
+        }
+
+
+        [HttpPost("create-job-and-run")]
+        public async Task<IActionResult> CreateJobAndRun([FromForm] JobRunForm form, CancellationToken ct)
+        {
+            if (form.inputFile1 is null || form.inputFile2 is null)
+            {
+                return BadRequest("Missing required files");
+            }
+
+            await using var s1 = form.inputFile1.OpenReadStream();
+            await using var s2 = form.inputFile2.OpenReadStream();
+
+
+            var (scenario,chain,job, run) = await _builder.CreateJobAndRunModelAsync(
+                form.ScenarioId.Value,
+                form.ChainId.Value,
+                form.ModelName,
+                s1,
+                s2,
+                GetRunsRoot(),
+                ct
+                );
+
+            return Ok(new
+            {
+                ScenarioId = scenario.Id,
+                ChainId = chain.Id,
+                JobId = job.JobId,
+                run.runId,
+                run.exitCode,
+                run.StandardOutput,
+                run.StandardError,
+                run.WorkDir,
+                run.ResultsDir,
+                Outputs = run.Outputs.Select(Path.GetFileName).ToArray()
+            });
+        }
 
     }
 }
